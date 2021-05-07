733406654c557ae6a8f13d110da97e3aec6b2f53
Mani Shankar <manishankar.offl@gmail.com>
2021-05-07 21:41:29 +0530

    fixed dictionary calls.

--- WeatherDashboard/WeatherApi/views.py
+++ WeatherDashboard/WeatherApi/views.py
@@ -1,6 +1,27 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from django.template import loader
+import python_weather
+import asyncio
+
+
+async def getweather(city):
+    # declare the client. format defaults to metric system (celcius, km/h, etc.)
+    client = python_weather.Client(format=python_weather.IMPERIAL)
+
+    # fetch a weather forecast from a city
+    weather = await client.find(city)
+
+    # # returns the current city temperature (int)
+    # print(weather.current.temperature)
+    #
+    # # get the weather forecast for a few days
+    # for forecast in weather.forecast:
+    #     print(str(forecast.date), forecast.sky_text, forecast.temperature)
+
+    # close the wrapper once done
+    await client.close()
+    return weather.current.temperature
 
 
 def home_page(request):
@@ -11,6 +32,7 @@ def home_page(request):
         'SAE': "https://www.sae.org/"
     }
     Domains = ["Google", "Instagram", "IEEE", "SAE"]
+
 
     context = {
         'Domains':  Domains,
