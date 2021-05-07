c05d94a36701150568d0018c6c073a3179abed8f
Mani Shankar <manishankar.offl@gmail.com>
2021-05-07 23:45:34 +0530

    Updated HTML and working Weather Info for Major Cities in India. Added list of other cities as well.

--- WeatherDashboard/WeatherApi/views.py
+++ WeatherDashboard/WeatherApi/views.py
@@ -3,6 +3,17 @@ from django.http import HttpResponse
 from django.template import loader
 import python_weather
 import asyncio
+import logging
+
+logging.basicConfig(filename='app.log', filemode='w', format='%(name)s - %(levelname)s - %(message)s')
+
+
+class WeatherInfo:
+    def __init__(self):
+        self.city = None
+        self.temperature = None
+        self.date = None
+        self.skytext = None
 
 
 async def getweather(city):
@@ -21,18 +32,40 @@ async def getweather(city):
 
     # close the wrapper once done
     await client.close()
-    return weather.current.temperature
+    return weather.current
+
+
+def parse_csv():
+    import csv
+    with open("WeatherDashboard/WeatherApi/artifacts/indian_cities.csv") as csvfile:
+        listcities = list()
+        csvread = csv.reader(csvfile, delimiter=",")
+        linecount = 0
+        for row in csvread:
+            if linecount > 3:
+                listcities.append(row[0])
+            linecount += 1
+    return listcities
 
 
 def home_page(request):
 
     list_of_cities = ["Chennai", "Delhi", "Mumbai", "Bangalore"]
+    # list_of_cities = parse_csv()
     tempdetails = dict()
     loop = asyncio.new_event_loop()
     asyncio.set_event_loop(loop)
     for city in list_of_cities:
-        temp = loop.run_until_complete(getweather(city))
-        tempdetails.update({city: temp})
+        try:
+            info = loop.run_until_complete(getweather(city))
+            data = WeatherInfo()
+            data.city = city
+            data.date = str(info.date)
+            data.skytext = info.sky_text
+            data.temperature = info.temperature
+            tempdetails.update({city: data})
+        except Exception as err:
+            logging.error(str("Missing City {}".format(city)))
 
     URL_list = {
         'Google': "https://www.google.com/",
