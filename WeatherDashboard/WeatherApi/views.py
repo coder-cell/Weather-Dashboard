c5a10fd597c74910cb850ab9f0828f0598c77272
Mani Shankar <manishankar.offl@gmail.com>
2021-05-07 20:10:51 +0530

    Template Render - WIP

--- WeatherDashboard/WeatherApi/views.py
+++ WeatherDashboard/WeatherApi/views.py
@@ -1,9 +1,22 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.template import loader
 
 
 def home_page(request):
-    return HttpResponse('<h>This is My Home Page</h>')
+    URL_list = [
+        "https://www.google.com/",
+        "https://www.instagram.com/",
+        "https://www.ieee.org/",
+        "https://www.sae.org/"
+    ]
+    Domains = ["Google", "Instagram", "IEEE", "SAE"]
+
+    context = {
+        'Domains':  Domains,
+        'URL': URL_list,
+    }
+    return HttpResponse(render(context, request))
 
 
 # Create your views here.
