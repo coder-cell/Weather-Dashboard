be19fc334f78c8709628c35d393f95af005d62f1
Mani Shankar <manishankar.offl@gmail.com>
2021-05-11 21:09:34 +0530

    Fixed the URL path issue, authentication succesful

--- openweatheraapi.py
+++ openweatheraapi.py
@@ -7,9 +7,9 @@ BASE_URL = "http://api.openweathermap.org/data/2.5/weather?"
 
 city_name = "Stuttgart"
 
-complete_url = BASE_URL + "appid=" + API_KEY + "&q=" + city_name
+complete_url = BASE_URL + "q=" + city_name + "&appid=" + API_KEY
 
 response = requests.get(complete_url)
 
-x = response.status_code
+x = response.json()
 print(x)
