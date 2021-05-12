2960e7e40fb0b9ba55d9ec7eb06e46ccde222ab8
Mani Shankar <manishankar.offl@gmail.com>
2021-05-12 23:28:45 +0530

    Added conversion of fractional time to actual time.

--- WeatherDashboard/WeatherApi/apis/__init__.py
+++ WeatherDashboard/WeatherApi/apis/__init__.py
@@ -1,7 +1,10 @@
 
 def convert_kelvintocelcius(kelvin):
-    return str(float(kelvin) - 273.15)
+    return str(round(float(kelvin) - 273.15, 2))
 
 
 def convertsecondstoutc(timezone):
-    return "UTC+" + str(float(timezone)/3600)
+    convertime = str(float(timezone)/3600)
+    hour = int(convertime)
+    minute = (convertime * 60) % 60
+    return "UTC + {}:{}".format(hour, minute)
