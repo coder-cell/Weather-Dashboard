bf271514e1c16becb036ae30e3ef6d147c6aead0
Mani Shankar <manishankar.offl@gmail.com>
2021-05-12 21:40:13 +0530

    Fixed library import error.

--- WeatherDashboard/WeatherApi/views.py
+++ WeatherDashboard/WeatherApi/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from django.template import loader
-import apis.pythonweatherapi as pwapi
+from .apis import pythonweatherapi as pwapi
 import asyncio
 import logging
 
