c49d97117b9f1191a94e342be3d2b07e3fd80178
Mani Shankar <manishankar.offl@gmail.com>
2021-05-15 22:16:06 +0530

    Fixed the minutes precision on UTC timezone.

--- WeatherDashboard/WeatherApi/apis/__init__.py
+++ WeatherDashboard/WeatherApi/apis/__init__.py
@@ -7,4 +7,4 @@ def convertsecondstoutc(timezone):
     convertime = float(float(timezone)/3600)
     hour = int(convertime)
     minute = (convertime * 60) % 60
-    return f"UTC + {hour:02d}:{int(minute)}"
+    return f"UTC + {hour:02d}:{int(minute):02d}"
